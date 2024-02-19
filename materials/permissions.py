107c75d2cc3277f6ddb56457aae6009da43efa08
ditsindrin <dmitriytsindrin1@gmail.com>
2024-02-19 23:21:15 +0300

    Home_25_1 DRF

--- materials/permissions.py
+++ materials/permissions.py
@@ -2,11 +2,13 @@ from rest_framework.permissions import BasePermission
 
 
 class IsModer(BasePermission):
+    """Права для группы модераторов"""
     def has_permission(self, request, view):
         return request.user.groups.filter(name='moderators').exists()
 
 
 class IsOwner(BasePermission):
+    """Права для владельцев"""
     def has_object_permission(self, request, view, obj):
         if request.user == obj.owner:
             return request.method in ('GET', 'POST', 'PATH', 'DELETE',)
