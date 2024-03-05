2771496fbeb4f22af3b756a5f636d46f7df6fbc0
ditsindrin <dmitriytsindrin1@gmail.com>
2024-03-05 22:27:25 +0300

    Home_24_2 DRF correct commands

--- materials/management/commands/add_course_and_lesson.py
+++ materials/management/commands/add_course_and_lesson.py
@@ -3,53 +3,53 @@ from django.core.management import BaseCommand
 from materials.models import Lesson, TrainingCourse
 
 test_course = {
-    "name": "Course 1 Test",
+    "title": "Course 1 Test",
     'description': "Test description for course 1",
-    "user_id": 1
+    "owner_id": 1
 }
 
 test_lessons = [
     {
         "user_id": 1,
-        "name": "Lesson 1",
+        "title": "Lesson 1",
         "description": "Decsription for lesson 1",
-        "link": "www.testlink1.com",
-        "course_id": 1,
+        "video_url": "www.testlink1.com",
+        "owner_id": 1,
     },
     {
         "user_id": 1,
-        "name": "Lesson 2",
+        "title": "Lesson 2",
         "description": "Decsription for lesson 2",
-        "link": "www.testlink2.com",
-        "course_id": 1,
+        "video_url": "www.testlink2.com",
+        "owner_id": 1,
     },
     {
         "user_id": 1,
-        "name": "Lesson 3",
+        "title": "Lesson 3",
         "description": "Decsription for lesson 3",
-        "link": "www.testlink3.com",
-        "course_id": 1,
+        "video_url": "www.testlink3.com",
+        "owner_id": 1,
     },
     {
         "user_id": 1,
-        "name": "Lesson 4",
+        "title": "Lesson 4",
         "description": "Decsription for lesson 4",
-        "link": "www.testlink4.com",
-        "course_id": 1,
+        "video_url": "www.testlink4.com",
+        "owner_id": 1,
     },
     {
         "user_id": 1,
-        "name": "Lesson 5",
+        "title": "Lesson 5",
         "description": "Decsription for lesson 5",
-        "link": "www.testlink5.com",
-        "course_id": 1,
+        "video_url": "www.testlink5.com",
+        "owner_id": 1,
     },
     {
         "user_id": 1,
-        "name": "Lesson 6",
+        "title": "Lesson 6",
         "description": "Decsription for lesson 6",
-        "link": "www.testlink6.com",
-        "course_id": 1,
+        "video_url": "www.testlink6.com",
+        "owner_id": 1,
     },
 ]
 
